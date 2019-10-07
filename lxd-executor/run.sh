7c633774d09e0f5c669c14bcc317c34b80fe3da0
J. Doe <email@example.org>
2019-10-07 14:22:20 +0200

    will it be over soon?

--- lxd-executor/run.sh
+++ lxd-executor/run.sh
@@ -4,6 +4,9 @@
 
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh # Get variables from base.
+# !!!!!
+CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID"
+
 
 lxc exec "$CONTAINER_ID" /bin/bash < "${1}"
 if [ $? -ne 0 ]; then
