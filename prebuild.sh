749aca55db68a42399b32f8772df05241c1d5eaf
J. Doe <email@example.org>
2019-10-07 14:49:05 +0200

    ansible CI test ;)

--- prebuild.sh
+++ prebuild.sh
@@ -1,24 +1,24 @@
-# export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
-# # Configure guest hostname
-# sudo bash -c 'echo 127.0.1.1 yunohost.yunohost.org yunohost >> /etc/hosts'
-# sudo hostname yunohost.yunohost.org
-# sudo bash -c 'echo yunohost.yunohost.org > /etc/hostname'
+# Configure guest hostname
+sudo bash -c 'echo 127.0.1.1 yunohost.yunohost.org yunohost >> /etc/hosts'
+sudo hostname yunohost.yunohost.org
+sudo bash -c 'echo yunohost.yunohost.org > /etc/hostname'
 
-# # Define root password
-# echo -e "yunohost\nyunohost" | sudo passwd root
+# Define root password
+echo -e "yunohost\nyunohost" | sudo passwd root
 
-# # Allow sudo removal (YunoHost use sudo-ldap)
-# export SUDO_FORCE_REMOVE=yes
+# Allow sudo removal (YunoHost use sudo-ldap)
+export SUDO_FORCE_REMOVE=yes
 
-# # Upgrade guest (done in install script)
-# sudo apt-get update
-# sudo apt-get -y --force-yes upgrade
-# sudo apt-get -y --force-yes dist-upgrade
+# Upgrade guest (done in install script)
+sudo apt-get update
+sudo apt-get -y --force-yes upgrade
+sudo apt-get -y --force-yes dist-upgrade
 
-# # Install YunoHost
-# wget https://raw.githubusercontent.com/YunoHost/install_script/stretch/install_yunohost -q -O /tmp/install_yunohost
-# sudo bash /tmp/install_yunohost -a -d unstable
+# Install YunoHost
+wget https://raw.githubusercontent.com/YunoHost/install_script/stretch/install_yunohost -q -O /tmp/install_yunohost
+sudo bash /tmp/install_yunohost -a -d unstable
 
-# # Cleanup
-# sudo apt-get clean -y
+# Cleanup
+sudo apt-get clean -y
