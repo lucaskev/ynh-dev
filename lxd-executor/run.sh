b802e4d3e7315ec7fc1ef89705269b4f172fe84a
J. Doe <email@example.org>
2019-10-07 11:21:47 +0200

    try w/ VPS

--- lxd-executor/run.sh
+++ lxd-executor/run.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
+# /opt/lxd-executor/run.sh
 
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh # Get variables from base.
 
-CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID"
-
 lxc exec "$CONTAINER_ID" /bin/bash < "${1}"
-
-
 if [ $? -ne 0 ]; then
     # Exit using the variable, to make the build as failure in GitLab
     # CI.
