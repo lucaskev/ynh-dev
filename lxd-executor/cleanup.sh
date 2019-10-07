22b5371181f5ce4b1d038ea6f58c2d491c5d254d
J. Doe <email@example.org>
2019-10-07 14:26:47 +0200

    fucking line missing in run.sh it seems...

--- lxd-executor/cleanup.sh
+++ lxd-executor/cleanup.sh
@@ -5,6 +5,9 @@
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${currentDir}/base.sh # Get variables from base.
 
+# !!!
+CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID"
+
 echo "Deleting container $CONTAINER_ID"
 
 lxc delete -f "$CONTAINER_ID"
