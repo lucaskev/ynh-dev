0aff3d74453e6006b2a6feb7a34813a7acf4985e
J. Doe <email@example.org>
2019-10-07 10:57:02 +0200

    first test w/ ansible

--- lxd-executor/base.sh
+++ lxd-executor/base.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-echo "DEBUG: /opt/lxd-executor/base.sh"
 
 CONTAINER_ID="runner-$CUSTOM_ENV_CI_RUNNER_ID-project-$CUSTOM_ENV_CI_PROJECT_ID-concurrent-$CUSTOM_ENV_CI_CONCURRENT_PROJECT_ID-$CUSTOM_ENV_CI_JOB_ID"
 
